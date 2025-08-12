1697042bcf8daf81218ca1716b83794451ca9ad9
CristianCalle <cristiancallepp@gmail.com>
2025-08-12 11:21:26 -0500

    se agrega funcion restar y se imprime resultado

--- pruebita.py
+++ pruebita.py
@@ -2,5 +2,12 @@ def sumar(num1:int, num2:int)-> int:
     respuesta = num1 + num2
     return respuesta
 
+def restar(num1:int, num2:int)-> int:
+    respuesta = num1 - num2
+    return respuesta
+
 a= sumar(1,2)
-print("sumita:", a)
+print("sumita: ", a)
+
+b = restar(1,3)
+print("restica: ", b)
